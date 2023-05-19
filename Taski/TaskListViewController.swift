2caa2485c9d7086f6a3f97342aa565edb064093d
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-05-19 19:51:23 +0300

    ALERT CONTROLLER USEAGE VERSION

--- Taski/TaskListViewController.swift
+++ Taski/TaskListViewController.swift
@@ -7,10 +7,6 @@
 
 import UIKit
 
-protocol TaskViewControllerDelegate {
-    func reloadData()
-}
-
 class TaskListViewController: UITableViewController {
 
     private let viewContext = (UIApplication.shared.delegate as! AppDelegate)
@@ -64,11 +60,9 @@ class TaskListViewController: UITableViewController {
     }
     
     @objc private func addTask() {
-        let taskVC = TaskViewController()
-        taskVC.delegate = self
-//        taskVC.modalPresentationStyle = .fullScreen // МОДАЛЬНОЕ представление на весь экран
-        present(taskVC, animated: true) // МОДАЛЬНОЕ представление  
-//        navigationController?.pushViewController(taskVC, animated: true) // Представление SHOW
+        showAlert(withTitle: "New task",
+                  andMessage: "Add task name"
+        )
     }
     
     private func fetchData() {
@@ -79,6 +73,47 @@ class TaskListViewController: UITableViewController {
             taskList = try viewContext.fetch(fetchRequest)
         } catch {
             print(error.localizedDescription)
+        }
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        // Инициализация контроллера
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // Определение действий
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            save(task)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        // Привязка действий к контроллеру
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        // Добавление текстового поля
+        alert.addTextField { textField in
+            textField.placeholder = "New task"
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    private func save(_ taskName: String) {
+        // Создаём экземпляр модели в контексте БД
+        let task = Task(context: viewContext)
+        // Передаем данные в атрибут объекта модели БД
+        task.title = taskName
+        taskList.append(task)
+        // Определяем индекс последнего элемента
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        // Сохранение если были изменения
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
         }
     }
 }
@@ -96,13 +131,5 @@ extension TaskListViewController {
         content.text = task.title
         cell.contentConfiguration = content
         return cell
-    }
-}
-
-// MARK: - TaskViewControllerDelegate
-extension TaskListViewController: TaskViewControllerDelegate {
-    func reloadData() {
-        fetchData()
-        tableView.reloadData()
     }
 }
