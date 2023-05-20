02922248f9b747d3811156621b4d3e0b45c07c3a
Vitaly Grinchik <vitaly.grinchik@gmail.com>
2023-05-20 22:57:10 +0300

    Released row edition on trailing swipe

--- Taski/TaskListViewController.swift
+++ Taski/TaskListViewController.swift
@@ -9,59 +9,83 @@ import UIKit
 
 class TaskListViewController: UITableViewController {
 
+    private enum TaskAction: String {
+        case addTask = "Add task"
+        case editTask = "Edit task"
+        case deleteTask = "Delete task"
+    }
+    
     // Доступ к зоне ОЗУ с объектами БД
     private let viewContext = StorageManager.shared.persistentContainer.viewContext
     
     private let cellID = "taskCell"
     
-    private var taskList = [Task]()
+    private var taskList = [Task]() {
+        didSet {
+//            updateData()
+        }
+    }
+    
+    lazy private var addButton = UIBarButtonItem(
+        barButtonSystemItem: .add,
+        target: self,
+        action: #selector(addTask)
+    )
+    
+    private let appColor = UIColor(
+        red: 21/255,
+        green: 101/255,
+        blue: 192/255,
+        alpha: 194/255
+    )
+    
+    private let editRowHandler: (() -> Void) = {}
+    
+    private let deleteRowHandler: (() -> Void) = {}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         // Регистрация ячеки таблицы
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        
         setupNaviBar()
         fetchData()
     }
     
     private func setupNaviBar() {
-        title = "Task List"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
         // Настройка представления navigation bar
         let naviBarAppearance = UINavigationBarAppearance()
-        naviBarAppearance.backgroundColor = UIColor(
-            red: 21/255,
-            green: 101/255,
-            blue: 192/255,
-            alpha: 194/255
-        )
         
-        // Стиль текста (аттрибуты)...
-        // ...для обычного размера текста ...
-        naviBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        // ... и для рекомендованного Apple крупного размера текста
-        naviBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true // Крупный шрифт названия
         
-        navigationController?.navigationBar.standardAppearance = naviBarAppearance
-        // The appearance settings for the Navigation bar when the edge of scrollable content aligns with the edge of the Navigation bar.
-        navigationController?.navigationBar.scrollEdgeAppearance = naviBarAppearance
-        // Добавляем системную кнопку в Navigation bar
-        navigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .add,
-            target: self,
-            action: #selector(addTask)
-        )
-        // Определяем цвет элементов Navigation Bar
+        // Цвета
         navigationController?.navigationBar.tintColor = .white
+        naviBarAppearance.backgroundColor = appColor
+        naviBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white] // для рекомендованного Apple крупного размера текста
+        naviBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white] // для обычного размера текста
         
+        // Стиль текста (аттрибуты)
+        navigationController?.navigationBar.standardAppearance = naviBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = naviBarAppearance
+        
+        // Добавляем системные кнопки "Addz"
+        navigationItem.rightBarButtonItem = addButton
+    
     }
     
     @objc private func addTask() {
-        showAlert(withTitle: "New task",
+        showDefaultAlert(forAction: .addTask, withTitle: "New task",
                   andMessage: "Add task name"
         )
+    }
+            
+    private func deleteTask(_ task: Task) {
+        
+    }
+    
+    private func editTask(_ task: Task) {
+        
     }
     
     private func fetchData() {
@@ -75,7 +99,7 @@ class TaskListViewController: UITableViewController {
         }
     }
     
-    private func showAlert(withTitle title: String, andMessage message: String) {
+    private func showDefaultAlert(forAction action: TaskAction, withTitle title: String, andMessage message: String) {
         // Инициализация контроллера
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         // Определение действий
@@ -83,14 +107,29 @@ class TaskListViewController: UITableViewController {
             guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
             save(task)
         }
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive)
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
-        // Привязка действий к контроллеру
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-        // Добавление текстового поля
-        alert.addTextField { textField in
-            textField.placeholder = "Task name"
+        
+        // Привязка действий к контроллеру в зависимости от действия с задачей
+        switch action {
+        case .addTask:
+            // Добавление текстового поля
+            alert.addTextField { textField in
+                textField.clearButtonMode = .whileEditing
+                textField.placeholder = "Task name"
+            }
+            alert.addAction(saveAction)
+        case .editTask:
+            alert.addTextField { textField in
+                textField.clearButtonMode = .whileEditing
+                textField.text = title
+            }
+            alert.addAction(saveAction)
+        case .deleteTask:
+            alert.addAction(deleteAction)
         }
+        
+        alert.addAction(cancelAction)
         
         present(alert, animated: true)
     }
@@ -101,20 +140,19 @@ class TaskListViewController: UITableViewController {
         // Передаем данные в атрибут объекта модели БД
         task.title = taskName
         taskList.append(task)
-        // Определяем индекс последнего элемента
+        // Определяем индекс последнего элемента для добавления в конец списка
         let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
-        
         tableView.insertRows(at: [cellIndex], with: .automatic)
         // Сохранение если были изменения
         if viewContext.hasChanges {
             do {
                 try viewContext.save()
-                
             } catch let error {
                 print(error.localizedDescription)
             }
         }
     }
+    
 }
 
 // MARK: - UITableViewDataSource
@@ -135,5 +173,22 @@ extension TaskListViewController {
 
 // MARK: - UITableViewDelegate
 extension TaskListViewController {
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let editRowAction = UIContextualAction(style: .normal, title: "Edit") { action, _, _ in
+            print("Editing...")
+        }
+        editRowAction.backgroundColor = appColor
+        
+        let deleteRowAction = UIContextualAction(style: .destructive, title: "Delete") { action, _, _ in
+            print("Deleting...")
+        }
+        
+        let swipeConfig = UISwipeActionsConfiguration(actions: [editRowAction, deleteRowAction])
+        // Не выполнять первое действия при полном свайпе
+        swipeConfig.performsFirstActionWithFullSwipe = false
+        
+        return swipeConfig
+    }
     
 }
